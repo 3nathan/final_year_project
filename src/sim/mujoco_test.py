67ae3e51c2e2e8b310bbea62973f5fb3fe07e53e
Nathan Dalton <nd221@ic.ac.uk>
2025-01-13 14:58:16 +0000

    add sim module and machine learning .py files

--- src/sim/mujoco_test.py
+++ src/sim/mujoco_test.py
@@ -12,20 +12,6 @@ import numpy as np
 from display import Display
 import matplotlib.pyplot as plt
 
-# xml = """
-# <mujoco>
-#   <worldbody>
-#     <camera name="view"/>
-#     <light name="top" pos="0 0 1"/>
-#     <body name="box_and_sphere" euler="0 0 -30">
-#       <joint name="swing" type="hinge" axis="1 -1 0" pos="-.2 -.2 -.2"/>
-#       <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
-#       <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
-#     </body>
-#   </worldbody>
-# </mujoco>
-# """
-# model = mujoco.MjModel.from_xml_string(xml)
 model = mujoco.MjModel.from_xml_path('../models/genghis.xml')
 data = mujoco.MjData(model)
 
@@ -33,15 +19,21 @@ data = mujoco.MjData(model)
 # scene_option = mujoco.MjvOption()
 # scene_option.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True
 
-duration = 3.8  # (seconds)
+duration = 10  # (seconds)
 framerate = 60  # (Hz)
 
 # Simulate and display video.
 frames = []
 mujoco.mj_resetData(model, data)
+
+data.ctrl[6] = 45
+# data.ctrl[7] = 45
+# data.ctrl[8] = 45
+
 with mujoco.Renderer(model) as renderer:
   while data.time < duration:
     mujoco.mj_step(model, data)
+
     if len(frames) < data.time * framerate:
       # renderer.update_scene(data, scene_option=scene_option)
       # renderer.update_scene(data, camera="zoom_plan")
