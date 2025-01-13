28310573013d8cbcd30330eeb0be84051ea50d0e
Nathan Dalton <nd221@ic.ac.uk>
2025-01-13 18:49:52 +0000

    move command line argument parsing to seperate arg parser module

---
+++ src/util/cli_arg_parser.py
@@ -0,0 +1,37 @@
+import argparse
+
+def parse():
+    parser = argparse.ArgumentParser(description="A MuJoCo reinforcement learning program")
+
+    parser.add_argument('-m', '--model',
+                        type=str,
+                        help="Path to the MuJoCo XML model file")
+
+    # need to implement once ML models are implemented
+    parser.add_argument('-t', '--train',
+                        type=bool,
+                        help="Train control model")
+
+    # need to implement once ML models are implemented
+    parser.add_argument('-c', '--control',
+                        type=str,
+                        help="Path to load ML model control file")
+
+    # need to implement image mode
+    parser.add_argument('-v', '--video',
+                        type=str,
+                        help="Render video or image from the simulation")
+
+    # need to implement image mode functionality
+    parser.add_argument('-T', '--time',
+                        type=int,
+                        help="Duration of video or time of screenshot simulation render")
+
+    # need to implement
+    parser.add_argument('-C', '--camera',
+                        type=str,
+                        help="Camera to use in the simulation render")
+
+    args = parser.parse_args()
+
+    return args
