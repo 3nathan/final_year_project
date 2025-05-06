fb2d0485516313ae065800f86b3da7d8cbb89866
Nathan Dalton <nd221@ic.ac.uk>
2025-05-06 16:57:14 +0100

    write bare bones for a Genghis agent for the rl scheme

---
+++ src/learning/agent.py
@@ -0,0 +1,22 @@
+# the inspiration for some of this code is taken from:
+# https://pytorch.org/tutorials/intermediate/mario_rl_tutorial.html
+
+class Genghis:
+    def __init__():
+        pass
+
+    def act(self, state):
+        """Given a state, choose an epsilon-greedy action"""
+        pass
+
+    def cache(self, experience):
+        """Add the experience to memory"""
+        pass
+
+    def recall(self):
+        """Sample experiences from memory"""
+        pass
+
+    def learn(self):
+        """Update online action value (Q) function with a batch of experiences"""
+        pass
