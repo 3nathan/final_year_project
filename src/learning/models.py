0edda707e9b2e0e94ea3638106f18cca622db036
Nathan Dalton <nd221@ic.ac.uk>
2025-05-30 17:30:04 +0100

    implement VAE network architecture also include function to construct a linear network

--- src/learning/models.py
+++ src/learning/models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.distributions import Normal
 
 from util.config import Config
 
@@ -13,20 +14,24 @@ CONFIG = Config()
 #       thetadot
 # it outputs an action distribution (joint torgues/target positions)
 
+def construct_linear(input_dim, hidden_dims):
+    layers = []
+    last_dim = input_dim
+    for curr_dim in hidden_dims:
+        layers.append(nn.Linear(last_dim, curr_dim))
+        layers.append(nn.LayerNorm(curr_dim))
+        layers.append(nn.LeakyReLU(0.1))
+        last_dim = curr_dim
+
+    return layers
+
 class GaitPolicy(nn.Module):
     def __init__(self, obs_dim, latent_dim, action_dim, hidden_dims=(512, 512, 512), activation=nn.LeakyReLU):
         super().__init__()
         input_dim = obs_dim + latent_dim
 
         # setup network based on given hidden dimensions
-        layers = []
-        last_dim = input_dim
-        for curr_dim in hidden_dims:
-            layers.append(nn.Linear(last_dim, curr_dim))
-            layers.append(nn.LayerNorm(curr_dim))
-            layers.append(activation(0.1))
-            last_dim = curr_dim
-        
+        layers = construct_linear(input_dim, hidden_dims)
         self.model = nn.Sequential(*layers)
 
         self.mean_head = nn.Linear(hidden_dims[-1], action_dim)
@@ -49,17 +54,30 @@ class GaitPolicy(nn.Module):
         state_dict = torch.load(path, map_location=torch.device('cpu'))
         self.load_state_dict(state_dict)
 
+class VAE(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dims=(256, 128), latent_dim, activation=nn.LeakyReLU):
+        super().__init__()
+        
+        encoder_layers = construct_linear(input_dim, hidden_dims)
+        decoder_layers = construct_linear(latent_dim, reversed(hidden_dims))
 
-# most of the following code is lifted from the pytorch reinforcement q learning tutorial:
-# # https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+        self.encoder = nn.Sequential(*layers)
+        self.decoder = nn.Sequential(*layers)
 
-# it references the original q learning paper:
-# https://arxiv.org/abs/1312.5602
+        self.mean_head = nn.Linear(hidden_dims[-1], latent_dim)
+        self.log_std_head = nn.Linear(hidden_dims[-1]. latent_dim)
 
-# replay memory (taken from pytorch tutorial)
-# stores transitions that the agent observes
-# allows later reuse
-# random sampling of this data build up decorrelated batches
-# shown that this greatly stabilises and improves DQN training procedure
+        self.output = nn.Linear(hidden_dims[0], output_dim)
 
-# TODO: in the future, implement the memory recall functionality for training the DQN model
+    def forward(self, x):
+        x = self.encoder(x)
+
+        mean = self.mean_head(x)
+        log_std = self.log_std_head(x)
+        log_std = torch.clamp(log_std, min=-20, max=2)
+        std = torch.exp(log_std)
+
+        sample = Normal(mean, std).sample()
+        output = self.decoder(sample)
+
+        return output, mean, std
