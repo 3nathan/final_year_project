d57ddb980a653c5b4daf8ae7cfc2bbc343015857
Nathan Dalton <nd221@ic.ac.uk>
2025-06-02 20:51:00 +0100

    implement all models for testing

--- src/learning/models.py
+++ src/learning/models.py
@@ -39,6 +39,8 @@ def construct_linear(input_dim, hidden_dims):
 class GaitPolicy(nn.Module):
     def __init__(self, obs_dim, latent_dim, action_dim, hidden_dims=(512, 512, 512), activation=nn.LeakyReLU):
         super().__init__()
+        self.obs_dim = obs_dim
+        self.latent_dim = latent_dim
         input_dim = obs_dim + latent_dim
 
         # setup network based on given hidden dimensions
@@ -49,6 +51,8 @@ class GaitPolicy(nn.Module):
         self.log_std_head = nn.Linear(hidden_dims[-1], action_dim)
 
     def forward(self, x):
+        obs = x[:self.obs_dim]
+        z = x[-self.latent_dim:]
         x = self.model(x)
 
         mean = torch.sigmoid(self.mean_head(x))
@@ -56,7 +60,7 @@ class GaitPolicy(nn.Module):
         log_std = torch.clamp(log_std, min=-20, max=2)
         std = torch.exp(log_std)
 
-        return mean, std
+        return mean, std, x
 
     def save_weights(self, path=str(CONFIG.ROOT)+"/models/neural_networks/quadruped.pth"):
         torch.save(self.state_dict(), path)
@@ -64,6 +68,70 @@ class GaitPolicy(nn.Module):
     def load_weights(self, path=str(CONFIG.ROOT)+"/models/neural_networks/quadruped.pth"):
         state_dict = torch.load(path, map_location=torch.device('cpu'))
         self.load_state_dict(state_dict)
+
+class DisconnectedPolicy(nn.Module):
+    def __init__(self, obs_dim, latent_dim, action_dim, hidden_dims=(512, 512, 512), activation=nn.LeakyReLU):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.latent_dim = latent_dim
+
+        self.left = GaitPolicy(obs_dim, latent_dim, action_dim//2, hidden_dims) 
+        self.right = GaitPolicy(obs_dim, 0, action_dim//2, hidden_dims) 
+
+    def forward(self, x):
+        obs = x[:self.obs_dim]
+        z = x[-self.latent_dim:]
+
+        left_mean, left_std, _ = self.left(x)
+        right_mean, right_std, _ = self.right(obs)
+
+        mean = torch.stack((left_mean, right_mean), dim=1)
+        std = torch.stack((left_std, right_std), dim=1)
+
+        return mean, std, _
+
+class DisconnectedCommPolicy(nn.Module):
+    def __init__(self, obs_dim, latent_dim, action_dim, hidden_dims=(512, 512, 512), activation=nn.LeakyReLU):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.latent_dim = latent_dim
+
+        self.left = GaitPolicy(obs_dim, latent_dim, action_dim//2, hidden_dims) 
+        self.right = GaitPolicy(obs_dim, latent_dim, action_dim//2, hidden_dims) 
+
+    def forward(self, x):
+        left_mean, left_std, _ = self.left(x)
+        right_mean, right_std, _ = self.right(x)
+
+        mean = torch.stack((left_mean, right_mean), dim=1)
+        std = torch.stack((left_std, right_std), dim=1)
+
+        return mean, std, _
+
+class EncodedCommPolicy(nn.Module):
+    def __init__(self, obs_dim, latent_dim, action_dim, hidden_dims=(512, 512, 512), encoded_dims=(256, 128, 1), activation=nn.LeakyReLU):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.latent_dim = latent_dim
+
+        self.left = GaitPolicy(obs_dim, latent_dim, action_dim//2, hidden_dims) 
+        self.right = GaitPolicy(obs_dim, encoded_dim, action_dim//2, hidden_dims) 
+
+        layers = construct_linear(hidden_dims[-1], encoded_dims)
+        self.encoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        obs = x[:self.obs_dim]
+
+        left_mean, left_std, high_lvl_features = self.left(x)
+
+        encoded = self.encoder(high_lvl_features)
+        right_mean, right_std, _ = self.right(torch.stack((obs, encoded), dim=1)
+
+        mean = torch.stack((left_mean, right_mean), dim=1)
+        std = torch.stack((left_std, right_std), dim=1)
+
+        return mean, std, _
 
 # this is a variational encoder used for multi-agent gait coordination in RL
 # it encodes input features into a latent variable z for inter-agent communication
@@ -81,40 +149,40 @@ class GaitPolicy(nn.Module):
 #       skips the processed features and transmits the infor the 'leader' has without the high level
 #       features that have been computed in the policy hidden layer
 
-class VAE(nn.Module):
-    def __init__(self, input_dim, output_dim, latent_dim, hidden_dims=(256, 128), activation=nn.LeakyReLU):
-        super().__init__()
-        encoder_layers = construct_linear(input_dim, hidden_dims)
-        decoder_layers = construct_linear(latent_dim, reversed(hidden_dims) + (output_dim,))
+# class VAE(nn.Module):
+#     def __init__(self, input_dim, output_dim, latent_dim, hidden_dims=(256, 128), activation=nn.LeakyReLU):
+#         super().__init__()
+#         encoder_layers = construct_linear(input_dim, hidden_dims)
+#         decoder_layers = construct_linear(latent_dim, reversed(hidden_dims) + (output_dim,))
 
-        self.encoder = nn.Sequential(*layers)
-        self.decoder = nn.Sequential(*layers)
+#         self.encoder = nn.Sequential(*layers)
+#         self.decoder = nn.Sequential(*layers)
 
-        self.mean_head = nn.Linear(hidden_dims[-1], latent_dim)
-        self.log_var_head = nn.Linear(hidden_dims[-1], latent_dim)
+#         self.mean_head = nn.Linear(hidden_dims[-1], latent_dim)
+#         self.log_var_head = nn.Linear(hidden_dims[-1], latent_dim)
 
-    def encode(self, x):
-        x = self.encoder(x)
+#     def encode(self, x):
+#         x = self.encoder(x)
         
-        mean = self.mean_head(x)
-        log_var = self.log_var_head(x)
-        # log_var = torch.clamp(log_var, min=-20, max=2)
+#         mean = self.mean_head(x)
+#         log_var = self.log_var_head(x)
+#         # log_var = torch.clamp(log_var, min=-20, max=2)
 
-        return mean, log_var
+#         return mean, log_var
 
-    def reparameterise(self, mean, log_var):
-        std = torch.exp(0.5*log_var)
-        return Normal(mean, std).sample()
+#     def reparameterise(self, mean, log_var):
+#         std = torch.exp(0.5*log_var)
+#         return Normal(mean, std).sample()
 
-    def decode(self, z):
-        return self.decoder(z)
+#     def decode(self, z):
+#         return self.decoder(z)
 
-    def forward(self, x):
-        mean, log_var = self.encode(x)
-        z = self.reparameterise(mean, log_var)
-        recon = self.decoder(z)
+#     def forward(self, x):
+#         mean, log_var = self.encode(x)
+#         z = self.reparameterise(mean, log_var)
+#         recon = self.decoder(z)
 
-        return recon, mean, log_var
+#         return recon, mean, log_var
 
     # TODO: investigate whether using deterministic is better
     #       use deterministic enables outputing the latent vector as the mean and disables
